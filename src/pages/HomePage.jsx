da42c6c4188c3f9016d2dfececedfeee75464bcf
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2024-08-05 20:33:12 +0200

    Cleanup

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,13 +3,7 @@ import Quiz from "../components/Quiz";
 import Navbar from "../components/Navbar";
 import Leaderboard from "../components/Leaderboard";
 
-//import { auth } from "../firebase/firebase";
-//import { useAuthState } from "react-firebase-hooks/auth";
-
-
-
 const HomePage = () => {
-  //const [user] = useAuthState(auth);
 
   return (
     <>
