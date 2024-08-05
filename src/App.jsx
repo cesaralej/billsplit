434e45acef71fe4618eda30f5456e3174c6dba30
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2024-08-05 20:16:31 +0200

    Housekeeping

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import HomePage from "./pages/HomePage";
 
 function App() {
+  console.log("%cHello! I'm here to help debug your code 🐛", "color:MediumSpringGreen");
   return (
     <div className="App">
       <HomePage />
---
+++ src/config/quizConfig.js
@@ -0,0 +1,5 @@
+export const TARGET_ANSWERS = [6, 7, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26, 27, 28, 29,];
+export const MIN_DIVISOR = 3;
+export const MAX_DIVISOR = 7;
+export const QUIZ_TIME = 60; // 60 seconds quiz time
+export const PENALTY_TIME = 3; // 3 seconds penalty duration
